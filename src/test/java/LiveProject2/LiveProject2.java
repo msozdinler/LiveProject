939f2f7e9966db278159823184afeac3e5b7a493
msozdinler <sozdinlermertc@gmail.com>
2023-03-04 00:44:40 -0500

    updated some scenarios

--- src/test/java/LiveProject2/LiveProject2.java
+++ src/test/java/LiveProject2/LiveProject2.java
@@ -2,6 +2,7 @@ package LiveProject2;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -189,6 +190,95 @@ After dragging the element to the target, you should see the element
         // driver.quit();
 
     }
+    @Test
+    void selectMenu() throws InterruptedException {
+        /**ID-12 Validate selecting drop down functionalities  */
 
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://demoqa.com/select-menu/");
+
+
+    }
+    @Test
+    void SliderDemo() throws InterruptedException {
+        /**ID-02 Validate the working of interaction button functionality*/
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://demoqa.com/slider/");
+
+        WebElement slider = driver.findElement(By.className("range-slider__wrap"));
+
+        int sliderWidth = slider.getSize().getWidth();
+
+        Actions move = new Actions(driver);
+        move.moveToElement(slider)
+                .clickAndHold()
+                .moveByOffset((int) (sliderWidth * 0.3), 0)
+                .release()
+                .perform();
+
+        Thread.sleep(3000);
+    }
+    @Test
+    void multipleSelectable(){
+
+        /**ID-07 Validate selecting multiple items with ctrl/cmd button and selecting the selected item*/
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://demoqa.com/selectable/");
+
+        // Find the selectable items
+        WebElement item1 = driver.findElement(By.xpath("//*[@id=\"verticalListContainer\"]/li[1]"));
+        WebElement item2 = driver.findElement(By.xpath("//*[@id=\"verticalListContainer\"]/li[2]"));
+        WebElement item3 = driver.findElement(By.xpath("//*[@id=\"verticalListContainer\"]/li[3]"));
+
+        Actions action = new Actions(driver);
+
+        action.keyDown(Keys.CONTROL)
+                .click(item1)
+                .click(item2)
+                .click(item3)
+                .click(item2)
+                .keyUp(Keys.CONTROL)
+                .perform();
+    }
+
+    @Test
+    void resizableMin() throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://demoqa.com/resizable/");
+
+        WebElement resizable = driver.findElement(By.xpath("//*[@id=\"resizable\"]/span"));
+
+
+        int initialWidth = resizable.getSize().getWidth();
+        int initialHeight = resizable.getSize().getHeight();
+
+        Actions resize = new Actions(driver);
+
+        resize.moveToElement(resizable)
+                .clickAndHold()
+                .moveByOffset(-(initialWidth - 300), - (initialHeight - 300))
+                .release()
+                .perform();
+
+        Thread.sleep(3000);
+
+    }
 
 }
+
+
