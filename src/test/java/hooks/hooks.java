cc443e30792cf270a0c78ae48d23679000e678ee
aselshaibekova <7aselshaibekova@gmail.com>
2023-03-22 00:40:26 +0400

    Asels TC

---
+++ src/test/java/hooks/hooks.java
@@ -0,0 +1,20 @@
+package hooks;
+
+import DriverPackage.BasicDriver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class hooks {
+    public class Hooks {
+
+        @Before // This method runs before every scenario
+        public void beforeScenario() {
+            System.out.println("Scenario has started");
+        }
+
+        @After // This method runs after every scenario
+        public void afterScenario() {
+            BasicDriver.quitDriver();
+            System.out.println("Scenario has ended");
+        }
+    }}
