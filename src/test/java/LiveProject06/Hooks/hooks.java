07b5c4d3144105ba83d1e4b5609968d55f896c79
msozdinler <sozdinlermertc@gmail.com>
2023-03-26 15:15:54 -0400

    6

---
+++ src/test/java/LiveProject06/Hooks/hooks.java
@@ -0,0 +1,20 @@
+package LiveProject06.Hooks;
+
+import DriverPackage.BasicDriver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+
+public class hooks {
+    public class Hooks {
+
+        @Before // This method runs before every scenario
+        public void beforeScenario() {
+            System.out.println("Scenario has started");
+        }
+
+        @After // This method runs after every scenario
+        public void afterScenario() {
+            BasicDriver.quitDriver();
+            System.out.println("Scenario has ended");
+        }
+    }}
